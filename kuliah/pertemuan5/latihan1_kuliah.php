6d5ac32cc06f62cb1d8bc01ad7af11440d951c44
faqih.213040033@mail.unpas.ac.id <faqih.213040033@mail.unpas.ac.id>
2022-03-12 14:02:21 +0700

    Ubah PHP

--- kuliah/pertemuan5/latihan1_kuliah.php
+++ kuliah/pertemuan5/latihan1_kuliah.php
@@ -1,31 +1,38 @@
 <?php  
 //ARRAY
 //Array adalah variabel yang dapat menampung lebih dari satu nilai sekaligus
+// elemen pada array boleh memiliki tipe data yang berbeda
+ 
+// $hari1 = "senin";
+// $hari2 = "selasa";
 
-$hari1 = "senin";
-$hari2 = "selasa";
+// $bulan1 = "Januari";
+// $bulan12 = "Desember";
 
-$bulan1 = "Januari";
-$bulan12 = "Desember";
-
-$mahasiswa15021345 = "Faqih";
+// $mahasiswa15021345 = "hiiqaff";
 
 
 //membuat Array
 $hari = ["senin","selasa","rabu","kamis","jum'at"]; // cara baru php versi 5
-$bulan = array("Januari","Februari","Maret");
- // cara lama
- // mencetak 1 elemen / nilai menggunakan indexnya, index dimulai dari 0
- $myArray = [100,"Hiiqaff",true];
+$bulan = array("Januari","Februari","Maret"); // cara lama
+ 
 
 echo $hari [2];
 echo "<br>";
 echo $bulan[1];
 echo "<hr>";
+ // mencetak 1 elemen / nilai menggunakan indexnya, index dimulai dari 0
+ // pasangan antara key dan value
+ $myArray = [100,"Hiiqaff",true];
 
+ echo $myArray[0];
+ echo "<hr>";
+ echo $myArray[1];
+ echo "<hr>";
 // mencetak semua elemen pada Array
 // var_dump() atau print_r()
 // khusus untuk debugging
+
 var_dump($hari);
 echo "<br>";
 print_r($bulan);
@@ -33,9 +40,11 @@ echo "<hr>";
 
 // Mencetak menggunakan looping
 // for
+
 // for($i = 0; $i < 4; $i++) {
 //     echo $hari[$i];
 //     echo "<br>";
+
 for($i = 0; $i < count($hari); $i++) {
     echo $hari[$i];
     echo "<br>";
@@ -43,20 +52,32 @@ for($i = 0; $i < count($hari); $i++) {
 echo "<hr>";
 
 //foreach (khusus untuk array)
+
 foreach($bulan as $b)  { // jamak = array as tunggal = variabel
  echo $b;
  echo "<br>";
 
 }
-
-foreach ($hari as $a => $b) {
-    echo "Key : $a, Value: $b";
-    echo "<br>";
-}
 echo "<hr>";
 
+// foreach ($hari as $key => $value) {
+//     echo "key: $key, value: $value";
+//     echo "<br>";
+// }
+//     echo "<hr>";
+
+foreach ($hari as $a => $b) {
+    echo "key: $a, value: $b";
+    echo "<br>";
+}
+    echo "<hr>";
+
 //memanipulasi isi array 
-// menambah elemen baru di akhir array
- 
-$hari 
+// menambahkan elemen baru di akhir array
+
+var_dump($hari);
+$hari[] = "Sabtu";
+$hari[] = "Minggu";
+echo"<hr>";
+var_dump($hari);
 ?>
